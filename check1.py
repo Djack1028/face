7f2c159b556b8647ced6ab7ecd268d0844dc2c21
Jamie <673400125@qq.com>
2019-12-12 11:45:54 +0800

    check

--- check1.py
+++ check1.py
@@ -7,8 +7,8 @@ def detect_face(img):
     #将测试图像转换为灰度图像，因为opencv人脸检测器需要灰度图像
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
  
-    #加载OpenCV人脸检测分类器Haar
-    face_cascade = cv2.CascadeClassifier('./haarcascade_frontalface_default.xml')
+    #加载OpenCV人脸检测分类器Haar   不能使用网上下载的文件，需要opencv自带的
+    face_cascade = cv2.CascadeClassifier('C:\Program Files\Anaconda3\Lib\site-packages\cv2\data\haarcascade_frontalface_default.xml')
  
     #检测多尺度图像，返回值是一张脸部区域信息的列表（x,y,宽,高）
     faces = face_cascade.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=5)
@@ -17,11 +17,13 @@ def detect_face(img):
     if (len(faces) == 0):
         return None, None
  
-    #目前假设只有一张脸，xy为左上角坐标，wh为矩形的宽高
-    (x, y, w, h) = faces[0]
+    # #目前假设只有一张脸，xy为左上角坐标，wh为矩形的宽高
+    # (x, y, w, h) = faces[0]
  
-    #返回图像的正面部分
-    return gray[y:y + w, x:x + h], faces[0]
+    # #返回图像的正面部分
+    # return gray[y:y + w, x:x + h], faces[0]
+
+    return faces
  
 #根据给定的（x，y）坐标和宽度高度在图像上绘制矩形
 def draw_rectangle(img, rect):
@@ -36,27 +38,33 @@ def predict(test_img):
     #生成图像的副本，这样就能保留原始图像
     img = test_img.copy()
     #检测人脸
-    face, rect = detect_face(img)
+    # face, rect = detect_face(img)
+    faces=detect_face(img)
 
     # 表情
-    label_text = '表情'
+    label_text = 'emotion'
  
-    # 在检测到的脸部周围画一个矩形
-    draw_rectangle(img, rect)
-    # 标出预测的名字
-    draw_text(img, label_text, rect[0], rect[1] - 5)
+    for rect in faces:
+        # face=gray[y:y + w, x:x + h]
 
-    print(face)
+        # 在检测到的脸部周围画一个矩形
+        draw_rectangle(img, rect)
+
+        # 标出预测的名字
+        draw_text(img, label_text, rect[0], rect[1] - 5)
+
+        # print(face)
+
     #返回预测的图像
     return img
  
 #加载测试图像
-test_img1 = cv2.imread("test_data/test1.jpg")
+test_img1 = cv2.imread("test_data/test1.png")
  
 #执行预测
 predicted_img1 = predict(test_img1)
  
 #显示两个图像
-cv2.imshow('表情', predicted_img1)
+cv2.imshow('emotion', predicted_img1)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
--- check2.py
+++ check2.py
@@ -2,11 +2,11 @@ import cv2
 import numpy as np
  
 # 运行之前，检查cascade文件路径是否在相应的目录下
-face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+face_cascade = cv2.CascadeClassifier('C:\Program Files\Anaconda3\Lib\site-packages\cv2\data\haarcascade_frontalface_default.xml')
+# eye_cascade = cv2.CascadeClassifier('C:\Program Files\Anaconda3\Lib\site-packages\cv2\data\haarcascade_eye.xml')
  
 # 读取图像
-img =cv2.imread('timg.jpg')
+img =cv2.imread("test_data/test2.png")
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
  
 # 检测脸部
@@ -18,9 +18,9 @@ for (x, y, w, h) in faces:
     roi_gray = gray[y: y + h, x: x + w]
     roi_color = img[y: y + h, x: x + w]
     
-    eyes = eye_cascade.detectMultiScale(roi_gray)
-    for(ex, ey, ew, eh) in eyes:
-        cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
+    # eyes = eye_cascade.detectMultiScale(roi_gray)
+    # for(ex, ey, ew, eh) in eyes:
+    #     cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
  
 cv2.imshow('img', img)
 cv2.waitKey(0)
